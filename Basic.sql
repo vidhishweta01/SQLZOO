bd20c89eca63b685c5798724dbb5eb789c6c0a21
Shweta Srivastava <73999611+vidhishweta01@users.noreply.github.com>
2021-03-04 21:59:20 +0530

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -140,3 +140,14 @@ SELECT matchid, player FROM goal
   SELECT id,stadium,team1,team2
   FROM game WHERE id LIKE '1012%'
 
+SELECT player,teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid) WHERE teamid = 'GER'
+  
+  SELECT team1, team2, player 
+  FROM game JOIN goal ON (id=matchid) WHERE player LIKE 'Mario%'
+  
+  SELECT player,teamid, coach, gtime
+  FROM goal JOIN eteam ON(teamid=id)
+ WHERE gtime<=10
+ 
+ SELECT mdate, teamname FROM game JOIN eteam ON (team1=eteam.id) WHERE coach = 'Fernando Santos'
